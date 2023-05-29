cda05c38d6d71ca99188530f5c6c709e656e7a28
Lauren Champs <laurenkchamps@gmail.com>
2023-05-30 09:13:17 +0930

    Add event listener for mouseover

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-let gridNumber = 16;
+let gridNumber = 64;
 
+const grid = document.querySelector('.grid');
 
 for (let i = 0; i < (gridNumber * gridNumber); i++) {
     const gridSquare = document.createElement('div');
     gridSquare.classList.add('grid-square');
-    
-    const grid = document.querySelector('.grid');
     grid.appendChild(gridSquare);
 };
 
-const grid = document.querySelector('.grid')
-
 grid.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
 grid.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
+
+const gridElement = document.querySelectorAll('.grid-square');
+
+gridElement.forEach(element => element.addEventListener('mouseenter', e => {
+    element.style.backgroundColor = 'black';
+}));

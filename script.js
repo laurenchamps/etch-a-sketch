6ebf990df038af41604c73f96b1195405615a24d
Lauren Champs <laurenkchamps@gmail.com>
2023-05-30 16:35:53 +0930

    Add functionality to only draw when mousedown

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
-let gridNumber = 64;
+let gridNumber = 99;
+let trigger = false;
 
 const grid = document.querySelector('.grid');
 
+// Create grid
 for (let i = 0; i < (gridNumber * gridNumber); i++) {
     const gridSquare = document.createElement('div');
     gridSquare.classList.add('grid-square');
     grid.appendChild(gridSquare);
 };
 
+// Size grid based on input
+
 grid.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
 grid.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
 
-const gridElement = document.querySelectorAll('.grid-square');
+// Get all squares in the grid
+const mouseTarget = document.querySelectorAll('.grid-square');
 
-gridElement.forEach(element => element.addEventListener('mouseenter', e => {
-    element.style.backgroundColor = 'black';
+// For each square, when mousedown set trigger to true to enable drawing
+mouseTarget.forEach(target => target.addEventListener('mousedown', (e) => {
+    trigger = true;
 }));
+
+// For each square, when mousedown set trigger to false to disable drawing
+mouseTarget.forEach(target => target.addEventListener('mouseup', (e) => {
+    trigger = false;
+}));
+
+// For each square in grid, apply 'activated' class on mouseover if drawing enabled
+mouseTarget.forEach(target => target.addEventListener('mouseover', (e) => {
+    if (trigger) {    
+        target.classList.add('activated');
+    };
+}));
+
+
+
+//Get grid size
+// const size = document.querySelector('#size').value;
+
+// console.log(size);
+
